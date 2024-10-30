3b6612da642e32ecf9b399b76c15e5e2848cbae2
Isaac Santana <isaacsantanadevr@gmail.com>
2024-10-30 10:44:15 -0300

    Update MapController.java

--- src/main/java/com/isaac/bahidro/model/MapController.java
+++ src/main/java/com/isaac/bahidro/model/MapController.java
@@ -8,10 +8,10 @@ public class MapController {
 
     // Mapeamento HTTP GET para a rota "/map". Quando essa URL for acessada,
     // o metodo 'showMap' sera chamado e ele retorna o nome da pagina a ser renderizada.
-    @GetMapping("/map")
+    @GetMapping("/")
     public String showMap() {
         // Retorna a string "map", que corresponde ao nome do arquivo HTML (map.html)
         // que sera renderizado pelo Thymeleaf no frontend.
-        return "map";
+        return "redirect:/map";
     }
 }
